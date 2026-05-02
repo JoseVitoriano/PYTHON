08df7772a54b93c405aada865594994bd3405322
José Vitoriano <vitorianoamorim7@gmail.com>
2026-05-01 23:16:29 -0300

    Desafio de modulação

---
+++ MODULARIZACAO/numeros.py
@@ -0,0 +1,6 @@
+import funcao
+
+num = int(input('Informe um numero para amalise: '))
+fat = funcao.fatorial(num)
+print(f'O fatorial do numero informado {num} e {fat}')
+print(f'O dobro do numero informado {num} e {funcao.dobro(num)}')
