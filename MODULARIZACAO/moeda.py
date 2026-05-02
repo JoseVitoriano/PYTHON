2a3e0726f3c64f26ac87bf5886551b435446c868
José Vitoriano <vitorianoamorim7@gmail.com>
2026-05-02 00:10:50 -0300

    modulação real

---
+++ MODULARIZACAO/moeda.py
@@ -0,0 +1,8 @@
+import teste
+
+num = float(input('Informe o seu salario: '))
+
+print(f'O salario com aumento e{teste.aumentar(num):.2f}')
+print(f'A metade do valor e: {teste.metade(num)}')
+print(f'O dobro do valor e: {teste.dobro(num)}')
+
