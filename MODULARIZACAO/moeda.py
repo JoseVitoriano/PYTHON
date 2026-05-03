223cee2777339620fb1628284f8d9858d63b9883
José Vitoriano <vitorianoamorim7@gmail.com>
2026-05-03 01:39:59 -0300

    alteração de modulação

--- MODULARIZACAO/moeda.py
+++ MODULARIZACAO/moeda.py
@@ -1,8 +1,8 @@
-import teste
+from teste import aumentar, dobro, metade
 
 num = float(input('Informe o seu salario: '))
 
-print(f'O salario com aumento e{teste.aumentar(num):.2f}')
-print(f'A metade do valor e: {teste.metade(num)}')
-print(f'O dobro do valor e: {teste.dobro(num)}')
+print(f'O salario com aumento e{aumentar(num)}')
+print(f'A metade do valor e: {metade(num)}')
+print(f'O dobro do valor e: {dobro(num)}')
 
